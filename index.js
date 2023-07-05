9c3bff81f08996c0db960b2eaf3fcbfabece30f8
user <mutukukennedy34@gmail.com>
2023-07-05 07:51:52 +0300

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+const jcontainer = document.getElementById("displaydrinks")
 
-fetch ("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink")
-.then (resp => resp.json())
-.then(margaritadata =>{
-    console.log(margaritadata)
-} )
+
+    fetch ("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic")
+    .then (resp => resp.json())
+    .then(alcoholicdata =>{
+   console.log(alcoholicdata);
+   
+   alcoholicdata.drinks.forEach(element => {
+    console.log(element);
+    alcoholcontaintainer(element)
+   })
+    .catch(error => {
+        console.error('Error:', error);
+      });
+})
+
+//Alcoholicdrinks()
+function alcoholcontaintainer(alcoholdrink){
+    const alcoholbutton = document.getElementById('alcoholbuttonid')
+ 
+    alcoholbutton.addEventListener('click' , ()=>{
+     
+    const alcoholdrinkname = document.createElement('p')
+    alcoholdrinkname.innerText = alcoholdrink.strDrink
+    console.log(alcoholdrinkname);
+    jcontainer.append(alcoholdrinkname)
+    
+    const alcoholdrinkimg = document.createElement('img')
+    alcoholdrinkimg.src = alcoholdrink.strDrinkThumb
+    jcontainer.append(alcoholdrinkimg)
+    })
+    
+}
